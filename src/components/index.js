a9f451e50b2686915ecacf9be64d99ef60ac1d36
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-06-15 21:22:18 +0530

    feat: Add Header, Navbar, Profile, Dashboard, Attendance, Concerns, Mentoring, Result, TimeTable, ToggleSwitch, StudentLogin, and TeacherLogin components

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Profile from "./Profile";
+import Dashboard from "./Dashboard";
+import Attendance from './Attendance';
+import Concerns from './Concerns';
+import Mentoring from './Mentoring';
+import Result from './Result';
+import TimeTable from './TimeTable';
+import ToggleSwitch from "./ToggleSwitch";
+import StudentLogin from './StudentLogin';
+import TeacherLogin from './TeacherLogin';
+
+export { Header, Navbar, Profile, Dashboard, Attendance, Concerns, Mentoring, Result, TimeTable, ToggleSwitch, StudentLogin, TeacherLogin };
