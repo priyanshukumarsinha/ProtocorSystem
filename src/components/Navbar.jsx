7e5e0b2be63c196e282e0ad4e8b70e1742070504
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-06-15 21:21:24 +0530

    chore: Update Navbar component to dynamically render menu items based on user type

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { makeActive } from '../store/NavbarSlice';
 
 const Navbar = () => {
+    const [menu, setMenu] = useState([])
+    const StudentNavbar = useSelector((state) => state.NavbarData.StudentNavbarData);
+    const TeacherNavbar = useSelector((state) => state.NavbarData.TeacherNavbarData);
+    const dispatch = useDispatch();
+    const isStudent = useSelector((state) => state.NavbarData.isStudent);
+
+    useEffect(() => {
+        const initialMenu = isStudent ? StudentNavbar : TeacherNavbar;
+        setMenu(initialMenu);
+    }, [isStudent]) 
+
+    const handleClick = (id) => {
+        const updatedMenu = menu.map((item) => {
+            if(item.id === id){
+                return ({...item, status: true})
+            }else{
+                return ({...item, status: false})
+            }
+        })
+        setMenu(updatedMenu);
+        dispatch(makeActive({id}));
+    }
+
   return (
     <nav className='w-full bg-black px-16 text-white sticky top-0'>
         <ul className='flex gap-5'>
-            <li className='py-5 px-4 bg-gray-700 cursor-pointer'>
-                {/* icon  */}
-                Dashboard
-            </li>
-            <li className='py-5 px-4 cursor-pointer'>
-                {/* icon  */}
-                Timetable
-            </li>
-            <li className='py-5 px-4 cursor-pointer'>
-                Result
-            </li>
-            <li className='py-5 px-4 cursor-pointer'>
-                Attendance
-            </li>
-            <li className='py-5 px-4 cursor-pointer'>
-                Mentoring
-            </li>
-            <li className='py-5 px-4 cursor-pointer'>
-                Concerns
-            </li>
+            
+            {
+                menu.map((item) => {
+                    return (
+                        <li 
+                        onClick={() => {handleClick(item.id)}}
+                        key={item.id} 
+                        className={`py-5 px-4 cursor-pointer ${item.status ? 'bg-gray-600/60' : ''}`}>
+                            {/* icon  */}
+                            {item.title}
+                        </li>
+                    )
+                })
+            }
         </ul>
     </nav>
   )
